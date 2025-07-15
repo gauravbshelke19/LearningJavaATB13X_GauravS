8dcd1679483e933df5581134a6c363d3b2f39d48
gauravshelke <gauravbshelke19@gmail.com>
2025-07-15 19:16:18 +0530

    new

---
+++ src/ex_02_Java_Basic/Lab_000.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basic;
+
+public class Lab_000 {
+    public static void main(String[] args){
+        int i=9;
+                int j=10;
+        System.out.printf("this is %d + %d",i,j);
+    }
+}
